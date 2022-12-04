18138685f9dca9a42767c45cb32f494f4b002b7b
Jener Sigua <jener.sigua31@gmail.com>
2022-12-04 17:41:18 +0800

    missing category icons fix

--- constant/category-icons.constant.ts
+++ constant/category-icons.constant.ts
@@ -1,6 +1,8 @@
 export const CATEGORY_ICON_MAPPING: {[key: string]: string} = {
 	'Mall': 'warehouse',
 	'Grocery': 'storefront-outline',
+	'Food':'food-outline',
+	'Hardware':'tools',
 	
 	'Frozen': 'fridge-alert-outline',
 	'Beverages': 'bottle-wine-outline',
